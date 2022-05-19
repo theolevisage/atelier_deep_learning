f30e4d3bfeebac23a1dd766c28bf26e201024146
theolevisage <theo.levisage@tutanota.com>
2022-05-19 09:30:02 +0200

    logistic regression lib added

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+
+X, y = datasets.load_iris(return_X_y=True)
+clf = LogisticRegression(random_state=0).fit(X, y)
 
 
 def print_hi(name):
